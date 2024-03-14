60e09c9df33c56f5fad0e23f8ebbd0f09de98f12
Veeti Ovaska <t1ovve00@students.oamk.fi>
2024-03-14 10:52:23 +0200

    nappi

--- DailySnapApp/App.js
+++ DailySnapApp/App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Avaa sovellus lol!</Text>
+      <TouchableOpacity>
+        <Text style={styles.kamera} >Avaa kamera</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +19,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  kamera: {
+    backgroundColor: 'black',
+    padding: 20,
+    borderRadius: 5,
+    color: 'white',
+    marginTop: 20,
   },
 });
