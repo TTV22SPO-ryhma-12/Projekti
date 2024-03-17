f891e9516e07da4fc5ef54a53a3783de3d2a1e16
TuomasAhola <127750665+TuomasAhola@users.noreply.github.com>
2024-03-17 12:32:24 +0200

    Update App.js

--- DailySnapApp/App.js
+++ DailySnapApp/App.js
@@ -1,19 +1,12 @@
 import { StyleSheet, View, TextInput, Button, StatusBar } from 'react-native';
-import { firestore, collection, addDoc, serverTimestamp, messages, getAuth } from './Firebase/FirebaseConfig';
 import { useState } from 'react';
 import { RegisterationForm } from './Components/Registeration';
-import { getFirestore } from 'firebase/firestore';
 import NavBar from './NavBar';
-import Constants from 'expo-constants';
 
 
 export default function App() {
   const [isSignedIn, setIsSignedIn] = useState(false);
   const [showSignIn, setShowSignIn] = useState(false);
-
-getFirestore();
-
-  const [newmessage, setnewmessage] = useState('')
 
   const handleSignOut = () => {
     setIsSignedIn(false);
@@ -26,7 +19,7 @@ getFirestore();
   return (
     <View style={styles.container}>
       <NavBar isSignedIn={isSignedIn} onToggleSignIn={handleToggleSignIn} onSignOut={handleSignOut} />
-      
+    
        <StatusBar backgroundColor="#000" barStyle="light-content" />
       {showSignIn && <RegisterationForm onSignIn={handleToggleSignIn} />}
     </View>
