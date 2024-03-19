f64cdaca60c5633c8bb6c3f9948fcac3cbaa3c75
simo vattulainen <simo.vattulainen@gmail.com>
2024-03-19 15:29:24 +0200

    Create ProfilePage.js

---
+++ DailySnapApp/screens/ProfilePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import react, { View } from 'react-native'
+
+export default function Profilepage(){
+
+    return(
+        <View>
+            <Text style={styles.navText}>Profile sivu</Text>
+        </View>
+    )
+}
