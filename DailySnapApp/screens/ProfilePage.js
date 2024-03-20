8f1ec7c125cd0b5ac8e17c2f9b30c3830b5fec64
simo vattulainen <simo.vattulainen@gmail.com>
2024-03-20 12:32:13 +0200

    profilepage luotu ja signout aloitus

--- DailySnapApp/screens/ProfilePage.js
+++ DailySnapApp/screens/ProfilePage.js
@@ -1,11 +1,37 @@
 import React from "react";
-import react, { View } from 'react-native'
+import react, { TouchableOpacity, View } from 'react-native'
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 export default function Profilepage(){
 
+    const handleSignOut = async () => {
+        await auth.signOut();
+    }
+
+    const Modal = () => {
+        Alert.alert("Auth App", "Do you really want to logout", [
+            {
+                text: "Cancel",
+                onpress: () => console.log("Cancel pressed"),
+            },
+            { text: "Logout", onPress: handleSignout },
+        ])
+    }
+
     return(
+        <View style={styles.container}>
+
+      
         <View>
-            <Text style={styles.navText}>Profile sivu</Text>
+            <Text style={styles.navText}>Tervetuloa profiiliisi</Text>
+       
+
+        <TouchableOpacity style={styles.button} onPress={handleSignOut}>
+        <Text style={{color: Colors.white, fontSize: 20 }}>Sign out</Text>
+        </TouchableOpacity>
         </View>
+    </View>
+
     )
 }
+
