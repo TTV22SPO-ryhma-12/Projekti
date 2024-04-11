adcf6be043ef3da5fb19abd362d5d888f35604f2
Veeti Ovaska <t1ovve00@students.oamk.fi>
2024-04-11 15:33:46 +0300

    Käyttäjän ja tietojen poisto

--- DailySnapApp/screens/Settings.js
+++ DailySnapApp/screens/Settings.js
@@ -1,12 +1,44 @@
-import { StyleSheet, View, Text, TextInput, Button, StatusBar } from 'react-native';
+import { StyleSheet, View, Text, TextInput, Button, StatusBar, Alert } from 'react-native';
+import React from 'react';
+import { deleteCurrentUser, auth, deleteUserStorageData } from '../Firebase/FirebaseAuth';
 
 
 
 export default function Settings() {
 
+    const handleDeleteAccount = async () => {
+        Alert.alert(
+            "Delete Account",
+            "Are you sure you want to delete your account?",
+            [
+                {
+                    text: "Cancel",
+                    onPress: () => console.log("Account deletion cancelled"),
+                    style: "cancel"
+                },
+                {
+                    text: "Delete Account",
+                    onPress: async () => {
+                        try {
+                            const userId = auth.currentUser.uid;
+                            await deleteUserStorageData(userId);
+                            await deleteCurrentUser();
+                            console.log("Account and all data has been deleted successfully");
+                        } catch (error) {
+                            console.error("Error deleting account:", error.message);
+                        }
+                    },
+                    style: "destructive"
+                },
+            ]
+        );
+
+    };
+
     return (
         <View style={styles.home}>
             <Text>Tervetuloa asetuksiin</Text>
+            <Button title="Delete Account" onPress={handleDeleteAccount} />
         </View>
     )
 }
