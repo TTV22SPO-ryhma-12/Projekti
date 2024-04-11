d9322e121c53b99a56d6db63be2f0368759cfea7
Veeti Ovaska <t1ovve00@students.oamk.fi>
2024-04-11 15:04:13 +0300

    Dotenv toimii

--- DailySnapApp/Firebase/FirebaseConfig.js
+++ DailySnapApp/Firebase/FirebaseConfig.js
@@ -4,17 +4,24 @@ import { getFirestore, collection, addDoc, serverTimestamp, } from "firebase/fir
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { getStorage, ref, uploadString } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  FIREBASEAPIKEY,
+  FIREBASEAUTHDOMAIN,
+  FIREBASEPROJECTID,
+  FIREBASESTORAGEBUCKET,
+  FIREBASEMESSAGINGSENDERID,
+  FIREBASEAPPID
 
-// Your web app's Firebase configuration
+} from "@env"
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyB-JwRhk7oL5TcymJtlQZAX2AxdRZl4X1g",
-  authDomain: "dailysnapapp-15efb.firebaseapp.com",
-  projectId: "dailysnapapp-15efb",
-  storageBucket: "dailysnapapp-15efb.appspot.com",
-  messagingSenderId: "63037632718",
-  appId: "1:63037632718:web:199ea99802d5b0f7d2209f"
+  apiKey: FIREBASEAPIKEY,
+  authDomain: FIREBASEAUTHDOMAIN,
+  projectId: FIREBASEPROJECTID,
+  storageBucket: FIREBASESTORAGEBUCKET,
+  messagingSenderId: FIREBASEMESSAGINGSENDERID,
+  appId: FIREBASEAPPID,
 };
 
 initializeApp(firebaseConfig);
